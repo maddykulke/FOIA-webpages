997c0a780f8bf1a743974a4bce00fa01dd9a8b5e
Loren Hinkson <loren.hinkson@gmail.com>
2018-07-09 17:04:43 -0400

    error checking and form dynamic additions

--- AuthScript.js
+++ AuthScript.js
@@ -119,16 +119,33 @@ function showDiv(divID) {
 
 
         hiddenDiv.style.display = 'none';
-
-
-function limitBirthdate() {
-    var today = new Date().toISOString().split('T')[0];
-    document.getElementById("dob").setAttribute('max', today);
+    }
 }
+
+
+function limitCurrentDates(dateArray) {
+  // Adapted from https://stackoverflow.com/questions/32378590/set-date-input-fields-max-date-to-today
+    var today = new Date().toISOString().split("T")[0];
+    for (var i = 0, dateLen = dateArray.length; i < dateLen; i++) {
+      document.getElementById(dateArray[i]).setAttribute("max", today);
+    }
+}
+
+
+
+function limitFuturedate(dateArray) {
+  // Adapted from https://stackoverflow.com/questions/32378590/set-date-input-fields-max-date-to-today
+    var today = new Date().toISOString().split("T")[0];
+    for (var i = 0, dateLen = dateArray.length; i < dateLen; i++) {
+      document.getElementById(dateArray[i]).setAttribute("min", today);
+    }
+}
+
+
 
 function customMessage(id, message){
 
-id.addEventListener("input", function (event) {
+  id.addEventListener("input", function (event) {
   if (id.validity.patternMismatch) {
     id.setCustomValidity(message);
   } else {
@@ -159,3 +176,69 @@ function acceptTerms(){
   }
 
 }
+
+
+
+function dynamicAdd(){
+    // Attribution: Dynamically adding duplicate (unique ID) form Div elements using javascript on 
+    // jsfiddle.net -  https://jsfiddle.net/phusick/Rceua/
+
+    var button = document.getElementById("newRecip");
+    
+    button.addEventListener("click", function() {
+        var sourceNode = document.getElementById("recipInfo");
+        var node = duplicateNode(sourceNode, ["id", "name", "placeholder"]);
+        
+        sourceNode.parentNode.parentNode.appendChild(node);
+    }, false);
+
+    
+    var counter = 0;
+    function duplicateNode(/*DOMNode*/sourceNode, /*Array*/attributesToBump) {
+        counter++;
+        var out = sourceNode.cloneNode(true);
+        if (out.hasAttribute("id")) { 
+          out["id"] = bump(out["id"]); 
+        }
+
+        var nodes = out.getElementsByTagName("*");
+        
+        for (var i = 0, len1 = nodes.length; i < len1; i++) {
+            var node = nodes[i];
+            for (var j = 0, len2 = attributesToBump.length; j < len2; j++) {
+                var attribute = attributesToBump[j];
+                if (node.hasAttribute(attribute)) {
+                    node[attribute] = bump(node[attribute]);
+                }
+            }
+        }
+        
+        function bump(str) {
+            return str + " " + counter;
+        }
+      
+        
+        var rem = document.createElement("input");
+        rem.type = "button";
+        rem.name = "removeRecip" + str(counter);
+        rem.value = "Remove Recipient";
+        rem.addEventListener("click", removeDiv(), false);
+        out.appendChild(rem);
+
+        function removeDiv(){
+            this.parentNode.parentNode.remove();
+        }
+
+        return out;
+    }
+
+    
+}
+
+
+
+datePickerId.max = new Date().toISOString().split("T")[0];
+
+
+
+
